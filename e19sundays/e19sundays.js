1d8cf50f613625d8b78c118d8c6ca692bc197d02
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2023-08-20 22:26:48 -0400

    e19 sunday & 1s

--- e19sundays/e19sundays.js
+++ e19sundays/e19sundays.js
@@ -10,6 +10,16 @@
 // A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
+const dict = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednsday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+};
+
 const startDate = new Date('1901-01-01');
 const endDate = new Date('2000-12-31');
 let sundays = 0;
@@ -19,9 +29,9 @@ console.log(`${startDate} was a ${startDate.getDay()}`);
 for (let i = startDate; i <= endDate; i.setDate(i.getDate() + 1)) {
     if (i.getDay() === 0 && i.getDate() === 1) {
         sundays++;
-        console.log(i);
+        console.log(`${dict[i.getDay()]} ${i.getDate()} ${i}`);
     }
 }
 
-console.log(`${startDate} was a ${startDate.getDay()}`);
+console.log(`${startDate} was a ${dict[startDate.getDay()]}`);
 console.log(sundays);
